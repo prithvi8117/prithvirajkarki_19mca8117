b691f608062fbc891254b46da9467316305c1f2e
Prithvi Raj Karki <uic.19mca8117@gmail.com>
2020-09-08 13:44:46 +0000

    Unnecessary commit

--- newJava.java
+++ newJava.java
@@ -3,5 +3,6 @@ class newJava
 	public static void main(String a[])
 	{
 		System.out.println("New Java Program for showing stash and restore");
+		System.out.println("This statement is going to be removed");
 	}
 }
