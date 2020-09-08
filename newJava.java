12402a05e94962e2f271f556cf502d68a35d7987
Prithvi Raj Karki <uic.19mca8117@gmail.com>
2020-09-08 13:21:28 +0000

    Good Commit

---
+++ newJava.java
@@ -0,0 +1,8 @@
+class newJava
+{
+	public static void main(String a[])
+	{
+		System.out.println("New Java Program for showing stash and restore");
+		System.out.println("This statement is going to be removed");
+	}
+}
